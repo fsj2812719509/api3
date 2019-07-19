68027bb7336a69e7c84639b53851b5c1be78a708
fsj2812719509 <2812719509@qq.com>
2019-07-19 10:58:45 +0000

    api3

--- app/Http/Controllers/Shop/IndexController.php
+++ app/Http/Controllers/Shop/IndexController.php
@@ -10,16 +10,15 @@ class IndexController extends Controller
 {
     //
     public function index(){
-        return '1';
         //跨域
-        /*header("Access-Control-Allow-origin:*");
+        header("Access-Control-Allow-origin:*");
         $data = \DB::table('1810shop_goods')->get();
-//        var_dump($data);exit;
+        var_dump($data);exit;
         $data = [
             "code"=>0,
             "msg"=>"select success",
             "data"=>$data
         ];
-        return json_encode($data,JSON_UNESCAPED_UNICODE);*/
+        return json_encode($data,JSON_UNESCAPED_UNICODE);
     }
 }
