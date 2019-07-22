2f149bb38a010d1b5047352bedd251e92828e576
fsj2812719509 <2812719509@qq.com>
2019-07-22 00:34:03 +0000

    api3

--- app/Http/Controllers/Shop/CartController.php
+++ app/Http/Controllers/Shop/CartController.php
@@ -6,10 +6,62 @@ use App\Model\CartModel;
 use App\Model\GoodsModel;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+class CartController extends Controller
+{
+    //
+    public function cartDo(Request $request){
 
-class CartController extends Controller{
-    public function cartDo(){
-        header('Access-Control-Allow-Origin:*');  //支持全域名访问，不安全，部署后需要固定限制为客户端网址
-        echo '1';
+        //跨域
+        header("Access-Control-Allow-origin:*");
+        echo '1';exit;
+
+
+        $all = $request->all();
+        $gid = $all['gid'];
+
+        //根据获取的id查出数据
+        $where = [
+            "gid"=>$gid
+        ];
+        $res = GoodsModel::where($where)->first();
+
+        //获取商品的名称，价钱，数量
+        $goods_name = $res['goods_name'];
+        $self_price = $res['self_price'];
+        $goods_img = $res['goods_img'];
+
+        $data = [
+            'goods_name'=>$goods_name,
+            'self_price'=>$self_price,
+            'goods_img'=>$goods_img,
+            'number'=>1
+        ];
+        //根据gid 查询数据库
+        $res3 = CartModel::where($where)->first();
+        if($res){
+            //没加入购物车一次就更改购物车数量
+            $number = $res3['number'];
+            $number = $number + 1;
+
+            $data = [
+                'number'=>$number
+            ];
+
+            $res4 = CartModel::where($where)->update($data);
+            var_dump($res4);
+        }else{
+            //加入购物车
+            $res = CartModel::insert($data);
+            var_dump($res);
+        }
+
+
+
+
+
+
+
+
+
     }
 }
